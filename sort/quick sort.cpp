7b417c993003910a6745a8c47a165610e8e5155c
Prashant Kumar <kumarprince2510@gmail.com>
2020-05-25 00:10:20 +0530

    quick sort code added
    It works by selecting a 'pivot' element from the array and partitioning the other elements into two sub-arrays, according to whether they are less than or greater than the pivot.

---
+++ sort/quick sort.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<stdlib.h>
+#include<stdio.h>
+#include<conio.h>
+
+using namespace std;
+
+void take_input(int *a,int n){
+
+    for (int i =0; i<n; i++)
+        cin>>a[i];
+
+}
+
+void display(int a[], int n){
+
+    for (int i=0;i < n; i++)
+        cout<<a[i]<<" ";
+}
+
+int find_partition(int a[],int le,int re){
+
+    int part_index=le;
+    int pivot_ele=a[le]; // left most value is considered as pivot element
+    int left_iterator=le;
+    int right_iterator=re;
+    while( left_iterator < right_iterator ){
+        while( pivot_ele >= a[left_iterator] )
+            left_iterator++;
+        while( pivot_ele < a[right_iterator] )
+            right_iterator--;
+        if( left_iterator < right_iterator )
+            swap(a[left_iterator],a[right_iterator]);  //swapping with inbuilt function
+    }
+    a[le]=a[right_iterator];
+    a[right_iterator]=pivot_ele;
+    return right_iterator;
+}
+
+ void quick_sort(int a[], int le, int re){
+    int part_tion;
+    if( le<re ){
+        part_tion=find_partition(a,le,re); // function to find partition element
+        quick_sort(a,le,part_tion-1);
+        quick_sort(a,part_tion+1,re);
+    }
+ }
+
+//driver code
+int main(){
+    int n;
+    cout<<"ENTER NO OF ELEMENT IN ARRAY: ";
+    cin>>n;
+    int arr[n];
+    cout<<"ENTERED ARRAY: ";
+    take_input(arr,n);
+    cout<<"\nENTERED ARRAY IS: ";
+    display(arr,n);
+    quick_sort(arr,0,n-1);
+    cout<<"\nTHE SORTED ARRAY IS: ";
+    display(arr,n);
+    return 0;
+}
