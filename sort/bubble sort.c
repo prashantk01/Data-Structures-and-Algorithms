2a7154c5c515d9f7ce13ca2f0544f825f17e8753
Prashant Kumar <kumarprince2510@gmail.com>
2020-06-06 10:19:41 +0530

    add improved version bubble sort code

---
+++ sort/bubble sort.c
@@ -0,0 +1,45 @@
+//modified version of bubble sort
+//o(n) for best case by using flag
+
+#include<stdlib.h>
+#include<stdio.h>
+void bubble_sort(int a[],int n){
+    for (int i=0;i<n;i++){
+        int flag=0;
+        for(int j=0;j<n-i;j++){
+                if(a[j] > a[j+1])
+                {
+                    int temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                    flag=1;
+                }
+        }
+        if(flag==0)
+            return;
+    }
+}
+
+void display(int a[], int n){
+    printf("\nEntered array is: ");
+    for (int i=0;i<n;i++){
+        printf("  %d",a[i]);
+    }
+    printf("\n");
+}
+
+int main(){
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter Array: ");
+    for(int i=0;i< n;i++){
+        scanf("%d",&arr[i]);
+    }
+    display(arr,n);
+    bubble_sort(arr,n);
+    printf("Sorted array is: \n");
+    display(arr,n);
+    return 0;
+}
